dfb9b75abc2c9986c10bb9f118ff3e91ff5d05e7
Dan <danricho@hotmail.com>
2016-07-11 20:14:23 +1000

    Updated comments

--- Joystick Gremlin/custom_buttons.py
+++ Joystick Gremlin/custom_buttons.py
@@ -3,22 +3,25 @@ from gremlin.input_devices import keyboard, macro
 import time
 from vjoy.vjoy import AxisName
 
+# used to allow the longer functions to not block the controllers while running
 import threading
-import logging
 
+# define the controllers
 right_t16000m = gremlin.input_devices.JoystickDecorator(name="T.16000M", device_id=(1325664945, 1), mode="Default")
 arduino_leonardo_1 = gremlin.input_devices.JoystickDecorator(name="Arduino Leonardo", device_id=(1092826752, 4), mode="Default")
 left_t16000m = gremlin.input_devices.JoystickDecorator(name="T.16000M", device_id=(1325664945, 0), mode="Default")
 
 # Logging Methods:
-# import logging
+import logging
 # logging.getLogger("system").debug("System debug info.")
 # logging.getLogger("user").debug("User debug info.")
 # or
 # gremlin.util.display_error("Error popup")
 
 def non_blocking_qt_gtfo(): # "QT GTFO" FUNCTION
-    #logging.getLogger("user").debug("GTFO running.")  
+    # This function warps in the direction the ship is facing immediately. Used in emergencies.
+    # logging.getLogger("user").debug("GTFO running.")  
+
     vjoy = gremlin.input_devices.VJoyProxy()
     joy = gremlin.input_devices.JoystickProxy()
 
@@ -38,7 +41,9 @@ def qt_gtfo(event, vjoy): # "QT GTFO" FUNCTION
       threading.Timer(0.1, non_blocking_qt_gtfo).start()  
     
 def non_blocking_all_cm_firing():
+    # This function fires the selected counter measure, toggles countermeasure, fires that counter measure and toggles back.
     # logging.getLogger("user").debug("All CM running.")
+    
     vjoy = gremlin.input_devices.VJoyProxy()
     joy = gremlin.input_devices.JoystickProxy()
     
@@ -68,7 +73,6 @@ def all_cm(event, vjoy): # "ALL CM" FUNCTION
       logging.getLogger("user").debug("'All C.M.' button pressed.")
       threading.Timer(0.1, non_blocking_all_cm_firing).start()
   
-
   
 ######################################################
 ##                                                  ##
@@ -77,6 +81,7 @@ def all_cm(event, vjoy): # "ALL CM" FUNCTION
 ##                                                  ##
 ######################################################
   
+# This macro toggles the chat window.
 macro_toggle_chat = macro.Macro()
 macro_toggle_chat.tap("F12")
 
@@ -87,6 +92,7 @@ def custom_1(event, vjoy):
       logging.getLogger("user").debug("Custom 1 button pressed.")
       macro_toggle_chat.run()
 
+# This macro is the salute emote. It must be run with the chat window hidden.
 macro_salute = macro.Macro()
 macro_salute.tap(macro.Keys.F12)
 macro_salute.tap(macro.Keys.Enter)
@@ -107,6 +113,7 @@ def custom_2(event, vjoy):
       logging.getLogger("user").debug("Custom 2 button pressed.")
       macro_salute.run()
 
+# This macro is the rude (flip the bird, etc) emote. It must be run with the chat window hiudden.
 macro_rude = macro.Macro()
 macro_rude.tap(macro.Keys.F12)
 macro_rude.tap(macro.Keys.Enter)
@@ -125,6 +132,7 @@ def custom_3(event, vjoy):
       logging.getLogger("user").debug("Custom 3 button pressed.")
       macro_rude.run()
 
+# This macro toggles video recording.
 macro_toggle_video_rec = macro.Macro()
 macro_toggle_video_rec.press("RAlt")
 macro_toggle_video_rec.tap("F7")
@@ -137,6 +145,7 @@ def custom_4(event, vjoy):
       logging.getLogger("user").debug("Custom 4 button pressed.")
       macro_toggle_video_rec.run()
 
+# This macro saves the last 5 minutes of video (or other duration in buffer).
 macro_save_video_last_five = macro.Macro()
 macro_save_video_last_five.press("RAlt")
 macro_save_video_last_five.tap("F8")
@@ -148,5 +157,3 @@ def custom_5(event, vjoy):
     if event.is_pressed:
       logging.getLogger("user").debug("Custom 5 button pressed.")
       macro_save_video_last_five.run()
-
-      
